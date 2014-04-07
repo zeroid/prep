1b19f63113975e55d8152718b342df1a84cf74f5
unknown <jsmith1@JSmith1-Westl1.am.trimblecorp.net>
2014-04-07 11:20:36 -0600

    Pushing new changes

--- source/prep/collections/MovieLibrary.cs
+++ source/prep/collections/MovieLibrary.cs
@@ -5,61 +5,72 @@ namespace prep.collections
 {
   public class MovieLibrary
   {
-    IList<Movie> movies;
+    private List<Movie> movies;
 
     public MovieLibrary(IList<Movie> list_of_movies)
     {
-      this.movies = list_of_movies;
+      this.movies = new List<Movie>(list_of_movies);
     }
 
     public IEnumerable<Movie> all_movies()
     {
-      return this.movies;
+      return this.movies.ToArray();
     }
 
     public void add(Movie movie)
     {
-      throw new NotImplementedException();
+        if (!movies.Contains(movie))
+        {
+          movies.Add(movie);
+        }
     }
-    
+
+    public IEnumerable<Movie> all_movies_published_by(ProductionStudio productionStudio)
+    {
+        return movies.FindAll(a => a.production_studio.Equals(productionStudio)).ToArray();
+    }
+
     public IEnumerable<Movie> all_movies_published_by_pixar()
     {
-      throw new NotImplementedException();
+        return all_movies_published_by(ProductionStudio.Pixar);
     }
 
     public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
     {
-      throw new NotImplementedException();
+        var result = new List<Movie>();
+        result.AddRange(all_movies_published_by(ProductionStudio.Pixar));
+        result.AddRange(all_movies_published_by(ProductionStudio.Disney));
+       return result.ToArray();
     }
 
     public IEnumerable<Movie> all_movies_not_published_by_pixar()
     {
-      throw new NotImplementedException();
+        return movies.FindAll(a => !a.production_studio.Equals(ProductionStudio.Pixar)).ToArray();
     }
 
     public IEnumerable<Movie> all_movies_published_after(int year)
     {
-      throw new NotImplementedException();
+        return movies.FindAll(a => a.date_published > new DateTime(year, 1, 1)).ToArray();
     }
 
     public IEnumerable<Movie> all_movies_published_between_years(int startingYear, int endingYear)
     {
-      throw new NotImplementedException();
+        return movies.FindAll(a => a.date_published >= new DateTime(startingYear, 1, 1) && a.date_published < new DateTime(endingYear, 12, 31)).ToArray();
     }
 
     public IEnumerable<Movie> all_kid_movies()
     {
-      throw new NotImplementedException();
+       return movies.FindAll(a => a.genre.Equals(Genre.kids));
     }
 
     public IEnumerable<Movie> all_action_movies()
     {
-      throw new NotImplementedException();
+      return movies.FindAll(a => a.genre.Equals(Genre.action));
     }
 
     public IEnumerable<Movie> sort_all_movies_by_title_descending()
     {
-      throw new NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public IEnumerable<Movie> sort_all_movies_by_title_ascending()
